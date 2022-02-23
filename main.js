eddd491e720191c6be14e7c5f8d12745261e6080
suri <hubcio092004@gmail.com>
2022-02-23 13:50:12 +0100

    code refactoring

--- main.js
+++ main.js
@@ -1,59 +1,40 @@
-document.body.style.backgroundColor ="black";
-document.body.style.color="white";
-document.body.style.position="relative";
+var POSTION_INTERVAL_MILLS = 10000;
+var BACKGROUND_COLOR_INTERVAL_MILLS = 20000;
 
-function circle(color){
-    document.getElementById("content").style.height ="150px";
-    document.getElementById("content").style.width="150px";
-    document.getElementById("content").style.position="absolute";
-    document.getElementById("content").style.backgroundColor=color;
-    //document.getElementById("content").style.display="inline-block";
-    document.getElementById("content").style.borderRadius="50%";
+var circle = document.getElementById("circle");
+
+setRandomElementBackgroundColor(circle);
+setRandomElementPosition(circle);
+
+setInterval(() => {
+  setRandomElementBackgroundColor(circle);
+}, BACKGROUND_COLOR_INTERVAL_MILLS);
+
+setInterval(() => {
+  setRandomElementPosition(circle);
+}, POSTION_INTERVAL_MILLS);
+
+function setRandomElementBackgroundColor(element) {
+  var elementColor = getRandomColor();
+  element.style.backgroundColor = elementColor;
 }
 
-// function circleShow(){
-//     document.getElementById("content").style.display="inline-block";
-// }
+function getRandomColor() {
+  var r = Math.floor(Math.random() * 255),
+    g = Math.floor(Math.random() * 255),
+    b = Math.floor(Math.random() * 255);
 
-// function circleHide(){
-//     document.getElementById("content").style.display="none";
-// }
-
-
-function screenSize(){
-    //console.log(window.innerWidth+" x "+window.innerHeight);
-    return [window.innerWidth, window.innerHeight]
+  var circleColor = "rgb(" + r + "," + g + "," + b + ")";
+  return circleColor;
 }
 
+function setRandomElementPosition(element) {
+  var screenWith = window.innerWidth,
+    screenHeight = window.innerHeight,
+    x = Math.floor(Math.random() * screenWith),
+    y = Math.floor(Math.random() * screenHeight);
 
-
-circleColor = "white";
-
-circle(circleColor);
-
-setInterval(function(){
-
-    r=Math.floor(Math.random() * 255);
-    g=Math.floor(Math.random() * 255);
-    b=Math.floor(Math.random() * 255);
-
-    circleColor="rgb("+r+","+g+","+b+")";
-
-}, 20000)
-
-setInterval(function(){
-
-    circle(circleColor);
-
-    [screenWith, screenHeight] = screenSize(); 
-    x=Math.floor(Math.random() * screenWith);
-    y=Math.floor(Math.random() * screenHeight);
-
-    // console.log(x,y);
-    
-    document.getElementById("content").style.top=y+"px";
-    document.getElementById("content").style.left=x+"px";
-
-
-}, 10000)
+  element.style.left = x + "px";
+  element.style.top = y + "px";
+}
 
